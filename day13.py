d7b3522fc11e86f2373c3621b80253046de078f0
Mark Wright <mwright@cygnacom.com>
2019-02-25 15:49:12 -0500

    Day 13 part two

--- day13.py
+++ day13.py
@@ -10,41 +10,35 @@ def parse(line):
     return layer, scanner_range
 
 
-class CaughtByScanner(Exception):
-    pass
+def faster_move_through_filewall(ranges):
+    # The back and forth movement of the scanner needn't be modelled directly.
+    # All that matters is when it is at 0.  If we look at scanner movement per
+    # range:
+    #
+    # 2: 0 1
+    # 3: 0 1 2 1
+    # 4: 0 1 2 3 2 1
+    #
+    # therefore, range -> period of repetition is (r-1)*2
+    # And if time % ((range-1)*2) == 0, we're "caught"
+
+    for layer, scanner_range in ranges.items():
+        picosecond = layer
+        if picosecond % ((scanner_range-1)*2) == 0:
+            yield layer
 
 
-def move_through_firewall(ranges, delay=0):
-    max_layer = max(ranges.keys())
+def find_delay_with_no_capture(ranges):
+    for delay in range(10000000):
+        caught = False
+        for layer, scanner_range in ranges.items():
+            picosecond = layer+delay
+            if picosecond % ((scanner_range-1)*2) == 0:
+                caught = True
+                break
+        if not caught:
+            return delay
 
-    scanner_states = dict(
-        (l, (0, 'down')) for l in ranges.keys())
-
-    times_caught = []
-
-    movement_plan = chain(
-        repeat(None, delay),
-        range(max_layer+1))
-    for current_layer in movement_plan:
-        if scanner_states.get(current_layer, (None, None))[0] == 0:
-            if delay == 0:
-                times_caught.append([current_layer, ranges[current_layer]])
-            else:
-                raise CaughtByScanner()
-
-        for layer, pos_and_dir in scanner_states.items():
-            scanner_range = ranges[layer]
-            if pos_and_dir == (0, 'up'):
-                scanner_states[layer] = (1, 'down')
-            elif pos_and_dir == (scanner_range-1, 'down'):
-                scanner_states[layer] = (scanner_range-2, 'up')
-            elif pos_and_dir[1] == 'down':
-                scanner_states[layer] = (pos_and_dir[0]+1, 'down')
-            elif pos_and_dir[1] == 'up':
-                scanner_states[layer] = (pos_and_dir[0]-1, 'up')
-
-    return times_caught
-    
 
 if __name__ == "__main__":
     sample_scanner_range_per_layer = dict(parse(l) for l in
@@ -53,25 +47,16 @@ if __name__ == "__main__":
             4: 4
             6: 4""".split("\n"))
 
-    assert 24 == sum(depth*scanner_range for depth, scanner_range
-        in move_through_firewall(sample_scanner_range_per_layer))
+    assert [0, 6] == list(
+        faster_move_through_filewall(sample_scanner_range_per_layer))
+    assert 24 == sum(depth*sample_scanner_range_per_layer[depth] for depth
+              in faster_move_through_filewall(sample_scanner_range_per_layer))
 
     with open('day13.txt') as f:
         scanner_range_per_layer = dict(parse(l) for l in f.readlines())
-    print("day 13 part 1:",
-          sum(depth*scanner_range for depth, scanner_range
-              in move_through_firewall(scanner_range_per_layer)))
 
-    # well, this isn't going to work...
-    #
-    #   I think that s[l] == 0 when time % ((range-1)*2) == 0.  So we can
-    #   convert all the scanners into that.
-    #
-    for delay in range(1, 100000):
-        print(delay, end="\r")
-        try:
-            move_through_firewall(scanner_range_per_layer, delay)
-            print("day 13 part 2:", delay)
-            break
-        except CaughtByScanner:
-            pass
+    print("day 13 part 1:",
+          sum(depth*scanner_range_per_layer[depth] for depth
+              in faster_move_through_filewall(scanner_range_per_layer)))
+    print("day 13 part 2:",
+        find_delay_with_no_capture(scanner_range_per_layer))
